41f37f0e92f28d72a9f6cecaeb0a28ef7e2a81eb
unknown <ashish.sahani@iitrpr.ac.in>
2022-05-26 16:35:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Camera_Check from './Camera_Check';
+import { BrowserRouter as Router, Route, Link, Routes} from "react-router-dom";
+import Face_Mesh_Test from './Face_Mesh_Test';
+import Hand_Mesh_Test from './Hand_Mesh_Test';
+import HandControlledCube from './HandControlledCube';
+import CharacterControlDemo from './CharacterControlDemo';
+import NavBar from './Navbar';
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <NavBar/>
+      <div className='content'>
+      <Router>
+        <Routes>
+          <Route exact path="/Camera_Check" element = {<Camera_Check/>}/>
+          <Route exact path="/Face_Mesh_Test" element = {<Face_Mesh_Test/>}/>
+          <Route exact path="/Hand_Mesh_Test" element = {<Hand_Mesh_Test/>}/>
+          <Route exact path="/HandControlledCube" element = {<HandControlledCube/>}/>
+          <Route exact path="/CharacterControlDemo" element = {<CharacterControlDemo/>}/>
+        </Routes>
+      </Router>
+      </div>
     </div>
   );
 }

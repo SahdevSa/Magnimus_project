44b958297d7d47f2bef0630ca21284752a896b4e
SahdevSa <2019csb1115@iitrpr.ac.in>
2022-06-03 11:07:16 +0530

    changed home

--- src/Home.js
+++ src/Home.js
@@ -1,57 +1,131 @@
+// import { useEffect } from "react";
+// import * as THREE from "three"
+// import {OrbitControls} from "three/examples/jsm/controls/OrbitControls"
+
+// function Home(){
+//     useEffect(()=>{
+//         const scene = new THREE.Scene();
+//         const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
+//         camera.position.z = 5
+//         const canvas = document.getElementById("myThreeJsCanvas");
+//         const renderer = new THREE.WebGLRenderer({canvas, antialias: true});
+//         renderer.setSize( window.innerWidth, window.innerHeight );
+//         document.body.appendChild( renderer.domElement );
+//         const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+//         ambientLight.castShadow = true;
+//         scene.add(ambientLight);
+
+        
+//         const boxGepmetry = new THREE.BoxGeometry(1,1,1);
+//         const boxMaterial = new THREE.MeshNormalMaterial();
+//         const boxMesh = new THREE.Mesh(boxGepmetry, boxMaterial);
+//         scene.add(boxMesh);
+
+//         const group1 = new THREE.Group();
+//         const cube1 = new THREE.Mesh(new THREE.BoxGeometry(1,1,1), new THREE.MeshNormalMaterial({color:0xf0da0f}));
+//         cube1.position.x = -1;
+//         const cube2 = new THREE.Mesh(new THREE.BoxGeometry(1,1,1), new THREE.MeshNormalMaterial({color:0xffda0f}));
+//         cube2.position.x = 3;
+//         group1.add(cube1);
+//         group1.add(cube2);
+//         scene.add(group1);
+
+//         const axisHelper = new THREE.AxesHelper()
+//         scene.add(axisHelper);
+
+//         boxMesh.scale.set(1,1, 2);
+
+//         const animate = () =>{
+            
+            
+//             boxMesh.rotation.x += 0.01;
+//             boxMesh.rotation.y += 0.01;
+//             boxMesh.position.x +=0.01;
+//             group1.rotation.x +=0.01;
+//             camera.lookAt(boxMesh.position);
+//             renderer.render( scene, camera );
+//             window.requestAnimationFrame( animate );
+//         };
+//         animate();
+//     }, [])
+
+//     return(
+//         <div>
+//             <canvas id= "myThreeJsCanvas"/>
+//         </div>
+//     )
+// }
+
+// export default Home
+
 import { useEffect } from "react";
-import * as THREE from "three"
+import * as THREE from "three";
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls"
 
 function Home(){
-    useEffect(()=>{
-        const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
-        camera.position.z = 5
-        const canvas = document.getElementById("myThreeJsCanvas");
-        const renderer = new THREE.WebGLRenderer({canvas, antialias: true});
+    let camera, scene, renderer, clock, joint; 
+    const onWindowResize =() =>{
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
         renderer.setSize( window.innerWidth, window.innerHeight );
-        document.body.appendChild( renderer.domElement );
-        const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-        ambientLight.castShadow = true;
-        scene.add(ambientLight);
+    }
 
-        
-        const boxGepmetry = new THREE.BoxGeometry(1,1,1);
-        const boxMaterial = new THREE.MeshNormalMaterial();
-        const boxMesh = new THREE.Mesh(boxGepmetry, boxMaterial);
-        scene.add(boxMesh);
+    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.01, 10 );
+    camera.position.set( 2, 2, - 2 );
+    clock = new THREE.Clock();
 
-        const group1 = new THREE.Group();
-        const cube1 = new THREE.Mesh(new THREE.BoxGeometry(1,1,1), new THREE.MeshNormalMaterial({color:0xf0da0f}));
-        cube1.position.x = -1;
-        const cube2 = new THREE.Mesh(new THREE.BoxGeometry(1,1,1), new THREE.MeshNormalMaterial({color:0xffda0f}));
-        cube2.position.x = 3;
-        group1.add(cube1);
-        group1.add(cube2);
-        scene.add(group1);
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0xffffff );
 
-        const axisHelper = new THREE.AxesHelper()
-        scene.add(axisHelper);
+    const light = new THREE.HemisphereLight( 0xbbbbff, 0x444422 );
+    light.position.set( 0, 1, 0 );
+    scene.add( light );
 
-        boxMesh.scale.set(1,1, 2);
+    // model
+    const loader = new GLTFLoader();
+    loader.load( 'https://threejs.org/examples/models/gltf/Soldier.glb', function ( gltf ) {
+
+    const model = gltf.scene;
+    
+    joint = model.getObjectByName( 'mixamorigLeftArm' );
+    joint.rotation.x = -0.2;
+    joint.rotation.y = -0.1;
+    joint.rotation.z = 0.1;
+        scene.add( model );
+    } );
+
+    useEffect(()=>{
+        const canvas = document.getElementById("myThreeJsCanvas");
+        renderer = new THREE.WebGLRenderer( { canvas, antialias: true } );
+        renderer.setPixelRatio( window.devicePixelRatio );
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        renderer.outputEncoding = THREE.sRGBEncoding;
+
+        window.addEventListener( 'resize', onWindowResize, false );
+
+        const controls = new OrbitControls(camera, renderer.domElement );
+        controls.target.set( 0, 1, 0 );
+        controls.autoRotate = true;
+        controls.update();
 
         const animate = () =>{
-            
-            
-            boxMesh.rotation.x += 0.01;
-            boxMesh.rotation.y += 0.01;
-            boxMesh.position.x +=0.01;
-            group1.rotation.x +=0.01;
-            camera.lookAt(boxMesh.position);
-            renderer.render( scene, camera );
-            window.requestAnimationFrame( animate );
+            requestAnimationFrame( animate );
+            const t = clock.getElapsedTime();
+    
+            if ( joint ) {
+                joint.rotation.z += Math.sin( t ) * 0.005;
+            }
+            controls.update();
+            renderer.render( scene, camera);
         };
         animate();
     }, [])
 
     return(
         <div>
-            <canvas id= "myThreeJsCanvas"/>
+            <canvas id= "myThreeJsCanvas">    
+            </canvas>
         </div>
     )
 }
